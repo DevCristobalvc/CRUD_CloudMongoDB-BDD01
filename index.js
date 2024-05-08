b1161b11bc4fd91507832fec993a5762821003c7
Cristobal Valencia Ceron <cristobal.valencia00@usc.edu.co>
2024-05-08 09:47:16 -0500

    Adjust View Form

--- index.js
+++ index.js
@@ -2,11 +2,20 @@
 
 const express = require('express');
 const { connectToMongoDB } = require('./database/mongoConnection');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+// Configuración para servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ruta principal, servirá el archivo index.html de la carpeta public
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Conectar a MongoDB Atlas
 connectToMongoDB()
